6ea6ffa595e3e197ae1fd081899935f38d348e65
Oleksandr Vasylenko <o.vasylenko@favbet.tech>
2025-01-02 23:41:06 +0100

    fix contact form

--- contactForm.js
+++ contactForm.js
@@ -1,6 +1,5 @@
 // Инициализация EmailJS
 (function() {
-    // emailjs.init('ox9_c_FHOvzXcimTx'); // Ваш ID пользователя, полученный в EmailJS
     emailjs.init({
         publicKey: 'ox9_c_FHOvzXcimTx',
     });
@@ -16,11 +15,16 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     const phone = document.getElementById('phone').value || 'Not provided';
     const organization = document.getElementById('organization').value || 'Not provided';
     const message = document.getElementById('message').value || 'No message';
-    console.log(name, email, phone, organization, message);
+    const responseMessage = document.getElementById('response-message');
+    const submitBtn = document.getElementById('submit-btn');
 
     // Проверка на обязательные поля
     if (!name || !email) {
-        document.getElementById('response-message').textContent = 'Please fill out all required fields.';
+        responseMessage.textContent = 'Please fill out all required fields.';
+        responseMessage.style.display = 'block'; // Показываем сообщение
+        setTimeout(() => {
+            responseMessage.style.display = 'none'; // Скрываем сообщение через 3 секунды
+        }, 3000);
         return;
     }
 
@@ -35,9 +39,21 @@ document.getElementById('contact-form').addEventListener('submit', function(even
 
     emailjs.send('service_oqyez5d', 'template_memt3a6', templateParams)
         .then(function(response) {
-            document.getElementById('response-message').textContent = 'Thank you for your message!';
+            responseMessage.textContent = 'Thank you for your message!';
+            responseMessage.style.color = '#0b8b70'; // Успешное сообщение
+            responseMessage.style.display = 'flex'; // Показываем сообщение
+            submitBtn.style.display = 'none';
+            setTimeout(() => {
+                responseMessage.style.display = 'none'; // Скрываем сообщение через 3 секунды
+                submitBtn.style.display = 'block';
+            }, 3000);
         }, function(error) {
-            document.getElementById('response-message').textContent = 'There was an error sending your message.';
+            responseMessage.textContent = 'There was an error sending your message!';
+            responseMessage.style.color = '#e74c3c'; // Сообщение об ошибке
+            responseMessage.style.display = 'block'; // Показываем сообщение
+            setTimeout(() => {
+                responseMessage.style.display = 'none'; // Скрываем сообщение через 3 секунды
+            }, 3000);
             console.error('Error:', error);
         });
 });
